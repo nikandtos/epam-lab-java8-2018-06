489a6484ef1fa41893a9ad600903f753e226c4bb
Nikita <oksapvn@gmail.com>
2018-06-04 18:16:07 +0300

    Third exercise done

--- src/test/java/lambda/part1/exercise/Exercise3.java
+++ src/test/java/lambda/part1/exercise/Exercise3.java
@@ -1,5 +1,6 @@
 package lambda.part1.exercise;
 
+import com.google.common.collect.FluentIterable;
 import lambda.data.Person;
 import org.junit.Test;
 
@@ -18,6 +19,8 @@ public class Exercise3 {
 
         // TODO использовать Arrays.sort + expression-lambda
 
+        Arrays.sort(persons,(Person o1, Person o2)->Integer.compare(o1.getAge(),o2.getAge()));
+
         assertArrayEquals(new Person[]{
             new Person("Иван", "Мельников", 20),
             new Person("Николай", "Зимов", 30),
@@ -32,6 +35,8 @@ public class Exercise3 {
 
         // TODO использовать Arrays.sort + statement-lambda
 
+        Arrays.sort(persons,(Person o1, Person o2)->o1.getLastName().compareTo(o2.getLastName())!=0?o1.getLastName().compareTo(o2.getLastName()):o1.getFirstName().compareTo(o2.getFirstName()));
+
         assertArrayEquals(new Person[]{
             new Person("Алексей", "Доренко", 40),
             new Person("Артем", "Зимов", 45),
@@ -45,7 +50,7 @@ public class Exercise3 {
         List<Person> persons = Arrays.asList(getPersons());
 
         // TODO использовать FluentIterable
-        Person person = null;
+        Person person = FluentIterable.from(persons).firstMatch(p-> p.getAge()==30).get();
 
         assertEquals(new Person("Николай", "Зимов", 30), person);
     }
