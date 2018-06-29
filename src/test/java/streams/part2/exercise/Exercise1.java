0bf73748cc2601ab843755dfb3f249efa87002ce
Nikita <oksapvn@gmail.com>
2018-06-29 16:41:33 +0300

    solved

--- src/test/java/streams/part2/exercise/Exercise1.java
+++ src/test/java/streams/part2/exercise/Exercise1.java
@@ -1,13 +1,21 @@
 package streams.part2.exercise;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lambda.data.Employee;
 import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import org.junit.Test;
-
-import java.util.*;
-
-import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings({"ConstantConditions", "unused"})
 public class Exercise1 {
@@ -17,7 +25,12 @@ public class Exercise1 {
         List<Employee> employees = getEmployees();
 
         // TODO реализация
-        Long hours = null;
+        Long hours = employees.stream()
+                              .mapToLong(e -> e.getJobHistory().stream()
+                                               .mapToLong(jhse ->
+                                                   jhse.getEmployer().equalsIgnoreCase("Epam")
+                                                       ? jhse.getDuration() : 0).sum())
+                              .sum();
 
         assertEquals(19, hours.longValue());
     }
@@ -27,12 +40,20 @@ public class Exercise1 {
         List<Employee> employees = getEmployees();
 
         // TODO реализация
-        Set<Person> workedAsQa = null;
+
+        Set<Person> workedAsQa = getEmployees().stream().filter(e -> e.getJobHistory().stream()
+                                                                      .anyMatch(
+                                                                          jobHistoryEntry -> jobHistoryEntry
+                                                                              .getPosition()
+                                                                              .equalsIgnoreCase(
+                                                                                  "QA")))
+                                               .map(Employee::getPerson).collect(
+                Collectors.toSet());
 
         assertEquals(new HashSet<>(Arrays.asList(
-                employees.get(2).getPerson(),
-                employees.get(4).getPerson(),
-                employees.get(5).getPerson()
+            employees.get(2).getPerson(),
+            employees.get(4).getPerson(),
+            employees.get(5).getPerson()
         )), workedAsQa);
     }
 
@@ -41,14 +62,16 @@ public class Exercise1 {
         List<Employee> employees = getEmployees();
 
         // TODO реализация
-        String result = null;
+        String result = employees.stream()
+                                 .map(e -> (CharSequence) e.getPerson().getFullName())
+                                 .collect(Collectors.joining("\n"));
 
         assertEquals("Иван Мельников\n"
-                + "Александр Дементьев\n"
-                + "Дмитрий Осинов\n"
-                + "Анна Светличная\n"
-                + "Игорь Толмачёв\n"
-                + "Иван Александров", result);
+            + "Александр Дементьев\n"
+            + "Дмитрий Осинов\n"
+            + "Анна Светличная\n"
+            + "Игорь Толмачёв\n"
+            + "Иван Александров", result);
     }
 
     @Test
@@ -56,16 +79,26 @@ public class Exercise1 {
     public void groupPersonsByFirstPositionUsingToMap() {
         List<Employee> employees = getEmployees();
 
+        Function<Employee,String> key = (employee -> employee.getJobHistory().get(0).getPosition());
+
+        Function<Employee,Set<Person>> addEmployee= (employee->new HashSet<>(Collections.singletonList(employee.getPerson())));
+
+        BinaryOperator<Set<Person>> compare = (right, left)->{
+            right.addAll(left);
+            return right;
+        };
         // TODO реализация
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees.stream().collect(
+            Collectors.toMap(key, addEmployee, compare));
 
         Map<String, Set<Person>> expected = new HashMap<>();
-        expected.put("QA", new HashSet<>(Arrays.asList(employees.get(2).getPerson(), employees.get(5).getPerson())));
+        expected.put("QA", new HashSet<>(
+            Arrays.asList(employees.get(2).getPerson(), employees.get(5).getPerson())));
         expected.put("dev", Collections.singleton(employees.get(0).getPerson()));
         expected.put("tester", new HashSet<>(Arrays.asList(
-                employees.get(1).getPerson(),
-                employees.get(3).getPerson(),
-                employees.get(4).getPerson()))
+            employees.get(1).getPerson(),
+            employees.get(3).getPerson(),
+            employees.get(4).getPerson()))
         );
         assertEquals(expected, result);
     }
@@ -75,60 +108,65 @@ public class Exercise1 {
     public void groupPersonsByFirstPositionUsingGroupingByCollector() {
         List<Employee> employees = getEmployees();
 
+        Function<Employee,String> key = (employee -> employee.getJobHistory().get(0).getPosition());
+
         // TODO реализация
-        Map<String, Set<Person>> result = null;
+
+        Map<String, Set<Person>> result = employees.stream().collect(Collectors
+            .groupingBy(key, Collectors.mapping(Employee::getPerson, Collectors.toSet())));
 
         Map<String, Set<Person>> expected = new HashMap<>();
-        expected.put("QA", new HashSet<>(Arrays.asList(employees.get(2).getPerson(), employees.get(5).getPerson())));
+        expected.put("QA", new HashSet<>(
+            Arrays.asList(employees.get(2).getPerson(), employees.get(5).getPerson())));
         expected.put("dev", Collections.singleton(employees.get(0).getPerson()));
         expected.put("tester", new HashSet<>(Arrays.asList(
-                employees.get(1).getPerson(),
-                employees.get(3).getPerson(),
-                employees.get(4).getPerson()))
+            employees.get(1).getPerson(),
+            employees.get(3).getPerson(),
+            employees.get(4).getPerson()))
         );
         assertEquals(expected, result);
     }
 
     private static List<Employee> getEmployees() {
         return Arrays.asList(
-                new Employee(
-                        new Person("Иван", "Мельников", 30),
-                        Arrays.asList(
-                                new JobHistoryEntry(2, "dev", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("Александр", "Дементьев", 28),
-                        Arrays.asList(
-                                new JobHistoryEntry(1, "tester", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("Дмитрий", "Осинов", 40),
-                        Arrays.asList(
-                                new JobHistoryEntry(3, "QA", "yandex"),
-                                new JobHistoryEntry(1, "QA", "mail.ru"),
-                                new JobHistoryEntry(1, "dev", "mail.ru")
-                        )),
-                new Employee(
-                        new Person("Анна", "Светличная", 21),
-                        Collections.singletonList(
-                                new JobHistoryEntry(1, "tester", "T-Systems")
-                        )),
-                new Employee(
-                        new Person("Игорь", "Толмачёв", 50),
-                        Arrays.asList(
-                                new JobHistoryEntry(5, "tester", "EPAM"),
-                                new JobHistoryEntry(6, "QA", "EPAM")
-                        )),
-                new Employee(
-                        new Person("Иван", "Александров", 33),
-                        Arrays.asList(
-                                new JobHistoryEntry(2, "QA", "T-Systems"),
-                                new JobHistoryEntry(3, "QA", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "EPAM")
-                        ))
+            new Employee(
+                new Person("Иван", "Мельников", 30),
+                Arrays.asList(
+                    new JobHistoryEntry(2, "dev", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "google")
+                )),
+            new Employee(
+                new Person("Александр", "Дементьев", 28),
+                Arrays.asList(
+                    new JobHistoryEntry(1, "tester", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "google")
+                )),
+            new Employee(
+                new Person("Дмитрий", "Осинов", 40),
+                Arrays.asList(
+                    new JobHistoryEntry(3, "QA", "yandex"),
+                    new JobHistoryEntry(1, "QA", "mail.ru"),
+                    new JobHistoryEntry(1, "dev", "mail.ru")
+                )),
+            new Employee(
+                new Person("Анна", "Светличная", 21),
+                Collections.singletonList(
+                    new JobHistoryEntry(1, "tester", "T-Systems")
+                )),
+            new Employee(
+                new Person("Игорь", "Толмачёв", 50),
+                Arrays.asList(
+                    new JobHistoryEntry(5, "tester", "EPAM"),
+                    new JobHistoryEntry(6, "QA", "EPAM")
+                )),
+            new Employee(
+                new Person("Иван", "Александров", 33),
+                Arrays.asList(
+                    new JobHistoryEntry(2, "QA", "T-Systems"),
+                    new JobHistoryEntry(3, "QA", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "EPAM")
+                ))
         );
     }
 }
